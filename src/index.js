505ece33edee285ea4322a6e6256f68068baf937
antbrothers <jianxi_lin@163.com>
2017-10-20 19:54:06 +0800

    重新整理目录结构

--- src/index.js
+++
@@ -1,7 +0,0 @@
-/**
- * Created by Administrator on 2017/10/18.
- */
-import React from "react"
-import ReactDOM from "react-dom"
-import router from './router'
-ReactDOM.render(router, document.getElementById("box"))
